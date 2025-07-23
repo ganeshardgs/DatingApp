7c22b00d464bbf2fb5cbb0000534d1991b514268
ganeshardgs <36593014+ganeshardgs@users.noreply.github.com>
2025-07-24 00:28:34 +0530

    Section2 Creating Angular frontend and add mkcert

---
+++ client/src/app/app.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  protected title = 'Dating App';
+  protected members = signal<any>([]);
+  private http = inject(HttpClient);
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers(){
+    try{
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }
+    catch (error) {
+      console.error('Error fetching members:', error);
+      throw error;
+    }
+  }
+}
